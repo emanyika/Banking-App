0eefea8f7b0d454496fcebc13c4ff55146dc5588
emanyika <emanyika@hotmail.co.uk>
2021-11-08 16:06:13 +0000

    First commit

---
+++ src/Bank.java
@@ -0,0 +1,12 @@
+public class Bank {
+	public static void main(String[] args) {
+		
+
+		account John = new account("John Smith", "A00001");
+		
+		
+		account Harry = new account("Harry Jones", "B00002");
+		Harry.showMenu();
+		}
+
+}
